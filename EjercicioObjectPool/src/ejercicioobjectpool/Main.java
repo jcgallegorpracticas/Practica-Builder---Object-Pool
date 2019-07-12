af92a542dafc7bed3f90cdcd218b4dde02936945
jcgallegor <jcgallegor@unal.edu.co>
2019-07-11 20:05:54 -0500

    practica builder y objecy pool

---
+++ EjercicioObjectPool/src/ejercicioobjectpool/Main.java
@@ -0,0 +1,26 @@
+package ejercicioobjectpool;
+
+import ejercicioobjectpool.JDBCConnectionPool;
+import java.sql.Connection;
+
+public class Main {
+  public static void main(String args[]) {
+    // Do something...
+    
+
+    // Create the ConnectionPool:
+    JDBCConnectionPool pool = new JDBCConnectionPool(
+      "org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb",
+      "sa", "secret");
+
+    // Get a connection:
+    Connection con = pool.checkOut();
+
+    // Use the connection
+    
+
+    // Return the connection:
+    pool.checkIn(con);
+ 
+  }
+}
